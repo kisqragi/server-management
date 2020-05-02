0d10cd5177c7bcda2caf56e6f3932349a6739fc1
kisqragi <kisaragi.sec@gmail.com>
2020-05-02 18:22:10 +0900

    ログイン履歴を確認し、slackへ通知するプログラムの追加

---
+++ server/main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+import requests, json, subprocess, ipaddress
+
+ALLOW_IP_LIST = ['x.x.x.x/x']
+WEBHOOK_URL = ""
+CHANNEL = "#hoge"
+USERNAME = "webhookbot"
+
+def check_login():
+    ret = '----- check_login -----\n'
+    deallow_ip_flag = True
+    cmd = 'grep Accepted /var/log/auth.log | tail -10'
+    proc = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+
+    for line in proc.stdout.decode('utf-8').split('\n'):
+        if not line:
+            ret += 'all ok\n'
+            break
+        line = line.split()
+        month = line[0]
+        day = line[1]
+        time = line[2]
+        user = line[8]
+        ip = ipaddress.ip_address(line[10])
+        deallow_ip_flag = True
+        for nw in ALLOW_IP_LIST:
+            nw = ipaddress.ip_network(nw)
+            if ip in nw:
+                deallow_ip_flag = False
+        if deallow_ip_flag:
+            ret += '{}-{}-{} {} {}\n'.format(month, day, time, user, ip)
+            break
+    return [deallow_ip_flag, ret]
+
+def doPost(url, channel, username, text):
+    payload = json.dumps({
+        "channel": channel,
+        "username": username,
+        "text": text,
+    }) 
+
+    requests.post(url, payload)
+
+if __name__ == '__main__':
+    text = ""
+    ret = check_login()
+
+    if ret[0]:
+        text = '<!channel>\n' + text
+
+    text += ret[1]
+
+    doPost(WEBHOOK_URL, CHANNEL, USERNAME, text)
